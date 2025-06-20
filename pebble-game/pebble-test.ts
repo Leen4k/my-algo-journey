13518137d6bb576077ee149afdc06209dbd427e3
wathnak <leen4kie@gmail.com>
2025-06-20 23:01:39 +0700

    feat: add simple pebble game test

---
+++ pebble-game/pebble-test.ts
@@ -0,0 +1,24 @@
+import { maxPebbleGameResult } from "./pebble-game";
+
+//TODO: add more tests
+describe("Pebble Game Maximum Result", () => {
+  test("should return maximum result for the example", () => {});
+
+  test("should handle simple two-element array", () => {});
+
+  test("should handle array with all positive number", () => {});
+
+  test("should handle array with negative number at the end", () => {});
+
+  test("should find optimal path avoiding negative numbers when possible", () => {});
+
+  test("should handle array where direct jump to end is optimal", () => {});
+
+  test("should handle case where we must visit negative intermediate squares", () => {});
+
+  test("should handle array with zeros", () => {});
+
+  test("should handle case where last element is very negative", () => {});
+
+  test("should handle longer array with mixed values", () => {});
+});
